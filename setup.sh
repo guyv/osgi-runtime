f8027e074cb716e8d70db5adcd775d2dea9fc56d
Guy Veraghtert <guy.veraghtert@esat.kuleuven.be>
2014-08-04 18:48:42 +0200

    update jars

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 rm -rf lib
 mkdir -p lib
 
-wget -O lib/org.eclipse.osgi.jar "http://download.eclipse.org/equinox/drops/R-Luna-201406061215/download.php?dropFile=org.eclipse.osgi_3.10.0.v20140606-1445.jar"
+wget -O lib/org.eclipse.osgi.jar "http://eclipse.mirror.kangaroot.net/equinox/drops/R-Luna-201406061215/org.eclipse.osgi_3.10.0.v20140606-1445.jar" 
 
 wget -O lib/org.apache.felix.gogo.runtime.jar "http://central.maven.org/maven2/org/apache/felix/org.apache.felix.gogo.runtime/0.12.1/org.apache.felix.gogo.runtime-0.12.1.jar" 
 wget -O lib/org.apache.felix.gogo.command.jar "http://central.maven.org/maven2/org/apache/felix/org.apache.felix.gogo.command/0.14.0/org.apache.felix.gogo.command-0.14.0.jar"
